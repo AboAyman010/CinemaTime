492af02b369243256944a59b971ca913ad862abf
Mohamed <ma770106@gmail.com>
2025-08-25 19:14:52 +0300

    update

--- CinemaTime/Areas/Admin/Controllers/MovieController.cs
+++ CinemaTime/Areas/Admin/Controllers/MovieController.cs
@@ -1,6 +1,7 @@
 ﻿using CinemaTime.DataAccess;
 using CinemaTime.Models;
 using CinemaTime.Models.ViewModel;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -29,11 +30,26 @@ namespace CinemaTime.Areas.Admin.Controllers
             return View(categoryWithSessionVM);
         }
         [HttpPost]
-        public IActionResult Create(Movie movie)
+        public IActionResult Create(Movie movie, IFormFile PosterUrl)
         {
-            _context.Movies.Add(movie);
-            _context.SaveChanges();
-            return RedirectToAction(nameof(Index));
+            //save img in wwwrot
+            if (PosterUrl is not null && PosterUrl.Length > 0)
+            {
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(PosterUrl.FileName);
+                var filePath =Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Images",
+                    fileName);
+
+                using (var stream = System.IO.File.Create(filePath))
+                {
+                    PosterUrl.CopyToAsync(stream);
+                }
+                movie.PosterUrl = fileName;
+                _context.Movies.Add(movie);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return BadRequest();
+         
         }
         public IActionResult Edit(int id)
         {
@@ -45,9 +61,40 @@ namespace CinemaTime.Areas.Admin.Controllers
             return View(Movies);
         }
         [HttpPost]
-        public IActionResult Edit(Movie movie)
+        public IActionResult Edit(Movie movie, IFormFile? PosterUrl)
         {
-            _context.Movies.Update(movie);
+            var moviesInDb=_context.Movies.AsNoTracking().FirstOrDefault(e => e.MovieId == movie.MovieId );
+            if(moviesInDb is null)
+            {
+                return BadRequest();
+            }
+            //save img in wwwrot
+            if (PosterUrl is not null && PosterUrl.Length > 0)
+            {
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(PosterUrl.FileName);
+                 
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Images",
+                    fileName);
+
+                using (var stream = System.IO.File.Create(filePath))
+                {
+                    PosterUrl.CopyTo(stream);
+                }
+                var OldFilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Images",
+                    moviesInDb.PosterUrl);
+                if (System.IO.File.Exists(OldFilePath))
+                {
+                    System.IO.File.Delete(OldFilePath);
+                }
+                movie.PosterUrl = fileName;
+            
+            }
+            else
+            {
+                movie.PosterUrl = moviesInDb.PosterUrl;
+            }
+
+                _context.Movies.Update(movie);
             _context.SaveChanges();
             return RedirectToAction(nameof(Index));
         }
